08554c5e0b83f140016571a563d0e31e98ff7191
cuongvt4510471 <cuongvt4510471@gmail.com>
2022-11-10 13:38:13 +0700

    fix: threads of callme

--- app/src/main/java/com/btcdteam/nhom1_demo/Cuongvtph28111.java
+++ app/src/main/java/com/btcdteam/nhom1_demo/Cuongvtph28111.java
@@ -1,7 +1,11 @@
 package com.btcdteam.nhom1_demo;
 
+import android.widget.Toast;
+
 public class Cuongvtph28111 {
     public void CallMe(){
         System.out.println("Hello world!");
+        //Fix
+        System.out.println("New world");
     }
 }
